4f0676b558ef1e274d7bcb9cb275e01090bb2e56
natasyazzhr1 <18219065@std.stei.itb.ac.id>
2021-03-29 22:30:31 +0700

    Update game.java

--- game.java
+++ game.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Game{
     private static int handSize = 7; // untuk jumlah kartu
-    private boolean isReverse; //untuk membalik
+    private static boolean isReverse; //untuk membalik
     // false = maju; true = mundur
     private int drawPenalty; //menambah jumlah draw (+2 / +4)
-    private int turn; //giliran keberapa
+    private static int turn; //giliran keberapa
     private player winner; //menampilkan nama pemenang
     private player player[]; // array nama pemain
 
@@ -20,24 +20,30 @@ public class Game{
 
     public void startGame(){
         //manggil setup game
+        SetupGame setupGame = new SetupGame();
+
+        setupGame.getPlayer();
+        setupGame.shuffleDeck();
+        setupGame.distributeCard();
+        setupGame.getTableCard();
+        setupGame.getFirstPlayer();
     }
 
-    public int step(){
-        this.turn = turn;
+    public static int step(int jmlhPemain){
         if (isReverse == true){
             return (turn + 1) % jmlhPemain; //manggil jumlah pemain
         } else if (isReverse == false){
-
+            return (turn + jmlhPemain - 1) % jmlhPemain;
         }
     }
 
     public void setTimer(){
     // ini pake thread
-    
+        
     }
 
     public player getWinner(){
-        if (Card.checkCard = 0){
+        if (player[].size() == 0){
             System.out.println(player.getName() + "has win the Game!");
         }
         return false;
@@ -51,8 +57,17 @@ public class Game{
         }
     }
 
-    public player[] listPlayer(){
-
+    public player[] listPlayer(int jmlhPemain){
+        for (int i = 0; i < jmlhPemain; i++){
+            System.out.println("Pemain " + i + ": ");
+            System.out.println("Jumlah Kartu: " + Card.getSize)
+            if (turn == ){
+                System.out.println('Sedang giliran')
+            } else if (turn == false){
+                System.out.println('Tidak sedang giliran')
+            }
+        }
+        
     }
 
     public String help(){
