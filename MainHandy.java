9d515b519b8e7cb3916cf9ee66a4ef0bade899c5
handyzulkarnain <68466944+handyzulkarnain@users.noreply.github.com>
2021-03-31 09:35:17 +0700

    minus dikit

---
+++ MainHandy.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// driver untuk
+// 1. minta jumlah pemain dan masukin nama pemain ke array;
+// 2. tentuin pemain pertama yang akan mulai secara acak;
+// 3. shuffleDeck
+public class MainHandy {
+
+    public static void main(String[] args) {
+        SetupGame setupGame = new SetupGame();
+        // input player
+        System.out.println("");
+        setupGame.getPlayer();
+        System.out.println("");
+
+        // ngeshuffle
+        System.out.println("Shuffling deck...");
+        setupGame.shuffleDeck();
+
+        // distribusi kartu
+        setupGame.distributeCard();
+
+        // tablecard
+        System.out.println("Table Card: ");
+        System.out.print(setupGame.getTableCard().getType() + " ");
+        System.out.print(setupGame.getTableCard().getColor() + " ");
+        System.out.print(setupGame.getTableCard().getValue() + " ");
+        System.out.println("");
+
+        // player pertama
+        System.out.println("Pemain pertama: " + setupGame.getFirstPlayer().getName());
+        System.out.println("");
+
+    }
+}
